0c1d5303ac9890d4496cb320669813103166f1dd
Eito Takahama <etakaham@c1r16s6.42tokyo.jp>
2023-06-17 21:42:58 +0900

    今後の予定のメモを追加

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: etakaham <etakaham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/17 12:07:57 by etakaham          #+#    #+#             */
-/*   Updated: 2023/06/17 21:38:32 by etakaham         ###   ########.fr       */
+/*   Updated: 2023/06/17 21:42:26 by etakaham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,8 @@ char	*get_next_line(int fd)
 	if (read_data == NULL)
 		return (NULL);
 	read(fd, read_data, BUFFER_SIZE);
+	// read_data を \n の前の文字列と後ろの文字列に分ける
+	// 前の文字列をresに、後ろの文字列をread_dataに格納する
+	// このときに使用する'\n'で分ける関数を作成する
 	return (read_data);
 }
