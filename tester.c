8090cfb94015d2cc0173f9d6fd24e41e00658e0e
fukayanegy <123365956+fukayanegy@users.noreply.github.com>
2023-07-04 14:58:04 +0900

    tester naosi

--- tester.c
+++ tester.c
@@ -5,7 +5,6 @@ int	main(void)
 {
 	int		fd;
 	char	*buffer;
-	// ssize_t	bytes_read;
 
 	buffer = malloc(1024);
 	fd = open("test.txt", O_RDONLY);
@@ -21,34 +20,5 @@ int	main(void)
 		free(buffer);
 	}
 	close(fd);
-
-	// char	s1[] = "abcdef\nhogehoge";
-	// char	s2[] = "ghijklm";
-	// char	*result_before;
-	// char	*result_after;
-
-	// printf("start\n");
-	// result_before = ft_copy_newline_before(s1);
-	// printf("-------------\n");
-	// printf("%s\n", result_before);
-	// printf("-------------\n");
-	// result_after = ft_copy_newline_after(s1);
-	// printf("-------------\n");
-	// printf("%s\n", result_after);
-	// printf("-------------\n");
-	// printf("start\n");
-	// // result = ft_strjoin(NULL, NULL);
-	// // printf("result is %s\n", result);
-	// printf("s1 len is %zu\n", ft_strlen(s1));
-	// printf("s2 len is %zu\n", ft_strlen(s2));
-	// result = ft_strchr(s1, '\n');
-	// printf("result is %s\n", result);
-
 	return (0);
-}
-#include <libc.h>
-
-__attribute__((destructor))
-static void destructor() {
-    system("leaks -q a.out");
 }
